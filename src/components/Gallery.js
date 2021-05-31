719457ed25b2834c81ba899864d1eb9eca7229a7
MarsPool.XYZ <marspoolxyz@gmail.com>
2021-05-31 14:56:26 +0400

    Added MarsPool Gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  { original:'https://ipfs.io/ipfs/QmcGW92x8oqUNWonQqL8yJ4XGNQ4ZvSEs1m7xiakSxbrxT', thumbnail:'https://ipfs.io/ipfs/QmcGW92x8oqUNWonQqL8yJ4XGNQ4ZvSEs1m7xiakSxbrxT',},
+  { original:'https://ipfs.io/ipfs/QmTvaiowYtgpvGDUbhyQPMrR7WZWLjWombijLaBzHgibyK', thumbnail:'https://ipfs.io/ipfs/QmTvaiowYtgpvGDUbhyQPMrR7WZWLjWombijLaBzHgibyK',},
+  { original:'https://ipfs.io/ipfs/QmUY27ANdANp26jXz1rX2D3n4FgVFzG8oapxUmUYDDDWqn', thumbnail:'https://ipfs.io/ipfs/QmUY27ANdANp26jXz1rX2D3n4FgVFzG8oapxUmUYDDDWqn',},
+  { original:'https://ipfs.io/ipfs/QmeKqzqSiXBDdRrbkCQuyqBcbahQbnRkn9FbdTzyVtywhT', thumbnail:'https://ipfs.io/ipfs/QmeKqzqSiXBDdRrbkCQuyqBcbahQbnRkn9FbdTzyVtywhT',},
+  { original:'https://ipfs.io/ipfs/QmejyTbYnwSMsG5VGUXt6sHrF2v4WeaCUxggBdAzdK7UBQ', thumbnail:'https://ipfs.io/ipfs/QmejyTbYnwSMsG5VGUXt6sHrF2v4WeaCUxggBdAzdK7UBQ',},
+  { original:'https://ipfs.io/ipfs/QmcN4rwxtHYujrrpG4fx4kUyhWKqccMWgA56PtMDhAfjfT', thumbnail:'https://ipfs.io/ipfs/QmcN4rwxtHYujrrpG4fx4kUyhWKqccMWgA56PtMDhAfjfT',},
+  { original:'https://ipfs.io/ipfs/QmeXTgQWDGQjYTZJBtp7QkpEBXFMK1VRa6cYvGhT4SqFrq', thumbnail:'https://ipfs.io/ipfs/QmeXTgQWDGQjYTZJBtp7QkpEBXFMK1VRa6cYvGhT4SqFrq',},
+  { original:'https://ipfs.io/ipfs/QmPgSZhV7rX6EVPoLpYC4PPtesFwLHqYy1Avjc22HQUeTQ', thumbnail:'https://ipfs.io/ipfs/QmPgSZhV7rX6EVPoLpYC4PPtesFwLHqYy1Avjc22HQUeTQ',},
+  { original:'https://ipfs.io/ipfs/Qmf3TXFj1XkRZTWEzzDBtEevMK6MEHBSJKbKJ1LqGNukM2', thumbnail:'https://ipfs.io/ipfs/Qmf3TXFj1XkRZTWEzzDBtEevMK6MEHBSJKbKJ1LqGNukM2',},
+  { original:'https://ipfs.io/ipfs/QmcpQTJCR4EhKVEBmbNmVPr3QpKruzqpdMoWgg8cdGXkp7', thumbnail:'https://ipfs.io/ipfs/QmcpQTJCR4EhKVEBmbNmVPr3QpKruzqpdMoWgg8cdGXkp7',},
+  { original:'https://ipfs.io/ipfs/Qmdk8VJN9aCF7KyoqTkKgj5qqXxSnNfvGoxRuqoZgcREBQ', thumbnail:'https://ipfs.io/ipfs/Qmdk8VJN9aCF7KyoqTkKgj5qqXxSnNfvGoxRuqoZgcREBQ',}
+ 
+];
+// Modify the front-end in the React JavaScript
+class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      message: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      <div style={{ "font-family": "sans-serif",color:"white" }}>
+      <div style={{ "font-size": "30px",color:"white"}}>
+      <p>MarsPool LAND NFT Gallery</p>
+      <p> Powered by <img height="46" width="100" src="images/dfx.gif" style={{"display":"inline"}} alt="Dfinity" /></p>
+      </div>
+      <ImageGallery items={images} />
+    </div>
+    </div>
+    );
+  }
+}
+export default Gallery;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+class Header extends Component {
+    render() {
+	    return (
+		<div className="row ">
+            <header>
+            <div className="header_bot">
+                <div className="wrapper">
+                <div className="logo"><
+                    a href="index.html"><img src="images/logo.png" alt="logo" /></a></div>
+                <div className="menu_area">
+                    <div id="navigation">
+                    <div id="navbtn">menu</div>
+                    <ul id="menu-nenu" className="nav-menu">
+                        <li><a href="http://dapp.marspool.xyz">HOME</a></li>
+                        <li><a href="http://dapp.marspool.xyz">BENEFITS</a></li>
+                        <li><a href="http://dapp.marspool.xyz">GALLERY</a></li>
+                        <li><a href="http://dapp.marspool.xyz">SEED TOKEN</a></li>
+                        <li><a href="http://dapp.marspool.xyz">MY WALLET</a></li>
+                        <li><a href="http://dapp.marspool.xyz">LIVE SALE</a></li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </header>            
+        </div>  
+	     );
+    }
+}
+export default Header;
